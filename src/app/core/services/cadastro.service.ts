238bb16fe15ee9f9c8de13df8020c17014cb7930
Ubirajara Neves <biraneves@outlook.com>
2024-02-21 12:48:23 -0300

    New methods in Cadastro service

--- src/app/core/services/cadastro.service.ts
+++ src/app/core/services/cadastro.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -14,5 +14,23 @@ export class CadastroService {
 
   cadastrar(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(`${this.apiUrl}/auth/cadastro`, usuario);
+  }
+
+  buscarCadastro(token: string): Observable<Usuario> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.get<Usuario>(`${this.apiUrl}/auth/perfil`, { headers });
+  }
+
+  editarCadastro(token: string, usuario: Usuario): Observable<Usuario> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    return this.http.patch<Usuario>(`${this.apiUrl}/auth/perfil`, usuario, {
+      headers,
+    });
   }
 }
