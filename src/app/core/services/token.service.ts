1eda6a54700910e7b78517c249386d033734e127
Ubirajara Neves <biraneves@outlook.com>
2024-02-21 11:46:25 -0300

    JWT Token service

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  salvarToken(token: string): void {
+    localStorage.setItem(KEY, token);
+  }
+
+  excluirToken(): void {
+    localStorage.removeItem(KEY);
+  }
+
+  retornarToken(): string | null {
+    return localStorage.getItem(KEY);
+  }
+
+  possuiToken(): boolean {
+    return !!this.retornarToken();
+  }
+}
